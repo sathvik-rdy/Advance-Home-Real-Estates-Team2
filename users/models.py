80a4296c154d28552d8d63e52a87427cc62a6823
Kaja Ali Mohiddin Sheik <sheik.kajaalimohiddin359@gmail.com>
2022-11-27 19:40:02 -0600

    modified models.py about phonenumber

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 
